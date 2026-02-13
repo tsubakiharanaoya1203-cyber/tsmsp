544a25d84af9d5ead19f1384f83efa005fb2dfb8
tsubakiharanaoya1203-cyber <tsubakiharanaoya1203@gmail.com>
2026-02-13 15:41:59 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME='tsms-cache-v1';
+const ASSETS=[
+  './',
+  './index.html',
+  './report.html',
+  './confirm.html',
+  './ops.html',
+  './manifest.json',
+  './sw.js',
+  './icon.png',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => (k===CACHE_NAME?null:caches.delete(k))))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => {});
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
